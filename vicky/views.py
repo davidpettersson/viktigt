1038279df9f6c7fc130e25d29c32bc77e5b52075
David Pettersson <david.pettersson@viendi.se>
2017-07-11 00:19:28 +0200

    Better exception handling

--- vicky/views.py
+++ vicky/views.py
@@ -3,6 +3,7 @@ import hashlib
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
 import xml.etree.ElementTree as ET
 
 from vicky.models import Alert
@@ -27,25 +28,27 @@ def alerts(req):
     message_sha1sum = summer.hexdigest()
     print('message_sha1sum: ' + message_sha1sum)
 
-    existing_alert = Alert.objects.get(message_sha1sum=message_sha1sum)
-
-    if existing_alert:
-        print('alert with identifier %s already presented and saved' % existing_alert.alert_id)
-        existing_alert.save()
-        return HttpResponse(status=202)
+    try:
+        existing_alert = Alert.objects.get(message_sha1sum=message_sha1sum)
+        if existing_alert:
+            print('alert with identifier %s already presented and saved' % existing_alert.alert_id)
+            existing_alert.save()
+            return HttpResponse(status=202)
+    except ObjectDoesNotExist:
+        print('alert does not exists in database')
 
     message_xml = req.body.decode('utf-8')
     print('message_xml: ' + message_xml)
 
 
-    root = ET.fromstring(message_xml_string)
+    root = ET.fromstring(message_xml)
     alert_id = root.find('cap:identifier', NS).text
     print('alert_id: ' + alert_id)
 
     alert = Alert()
     alert.alert_id = alert_id
     alert.message_sha1sum = message_sha1sum
-    alert.message_xml = message_xml_string
+    alert.message_xml = message_xml
     alert.save()
 
     return HttpResponse(status=201)
