4049c26e431c3594426023457626d75c7b5af520
David Pettersson <david.pettersson@viendi.se>
2017-07-21 22:21:38 +0200

    Implemented basic validation of the CAP message according to the schema.

--- vicky/views.py
+++ vicky/views.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 from pprint import pprint
 
 import dateutil.parser
@@ -8,11 +9,15 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
 import xml.etree.ElementTree as ET
 
+from subprocess import Popen, PIPE
+
 from vicky.models import Alert
 
 NS = {
     'cap': 'urn:oasis:names:tc:emergency:cap:1.2'
 }
+
+XMLLINT_BINARY = 'xmllint'
 
 def index(req):
     alerts = Alert.objects.all().order_by('-alert_sent')
@@ -29,10 +34,27 @@ def alert_details(req, id):
     }
     return render_to_response('vicky/alert_detail.html', context)
 
+
+
+def _lint_passed(body):
+    args = [XMLLINT_BINARY, '--schema', 'vicky' + os.sep + 'schemas' + os.sep + 'CAP-v1.2.xsd', '--noout', '-']
+    print(' '.join(args))
+    process = Popen(args, stdin=PIPE)
+    process.stdin.write(body)
+    process.stdin.close()
+    process.wait()
+    rc = process.returncode
+    print('return code is ' + str(rc))
+    return rc == 0
+
 @csrf_exempt
 def alerts(req):
     if not req.method in ['POST', 'PUT']:
         return HttpResponse(status=405)
+
+    if not _lint_passed(req.body):
+        print('lint did not pass')
+        return HttpResponse(status=400)
 
     # Check if we have seen this once before
     summer = hashlib.sha1()
@@ -50,7 +72,7 @@ def alerts(req):
         print('alert does not exists in database')
 
     message_xml = req.body.decode('utf-8')
-    print('message_xml: ' + message_xml)
+    # print('message_xml: ' + message_xml)
 
     root = ET.fromstring(message_xml)
 
